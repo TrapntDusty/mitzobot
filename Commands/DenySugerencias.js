f061d0b21c42905591c92ad1fa2ca3491a55bf0a
Dusty <57601174+TrapntDusty@users.noreply.github.com>
2021-07-29 00:33:05 -0500

    Update DenySugerencias.js

--- Commands/DenySugerencias.js
+++ Commands/DenySugerencias.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'DenySugerencias',
     description: 'Denegar una sugerencia',
     async execute(client,message,args,discord){
-        if(!message.member.roles.cache.has(`824186297513541652`)) return; //rol admin
+        if(!message.member.roles.cache.has(`ROL ID`)) return; //rol admin
 
         const messageID = args[0];
         const denyQuery = args.slice(1).join(" ");
@@ -13,7 +13,7 @@ module.exports = {
         if(!denyQuery) return message.reply("porfavor especifica la razon del rechazo")
 
         try{
-            const suggestionChannel = message.guild.channels.cache.get(`828095608316362804`) //canal sugerencias
+            const suggestionChannel = message.guild.channels.cache.get(`CHANNEL ID`) //canal sugerencias
             const suggestedEmbed = await suggestionChannel.messages.fetch(messageID);
             const data = suggestedEmbed.embeds[0];
             const deniedEmbed = new MessageEmbed()
